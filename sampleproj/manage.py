9fb425aa5a6f9156e53457c56b5d908ab9b629f8
Aditya <adikumar@uw.edu>
2018-08-15 09:58:36 -0700

    switched internal manage.py default settings to travis
    This will be used for migrations so travis builds stop erroring

--- sampleproj/manage.py
+++ sampleproj/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sampleproj.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sampleproj.settings.travis")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
---
+++ sampleproj/settings/travis.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import
+import os
+from .base import *
+ALLOWED_HOSTS = []
