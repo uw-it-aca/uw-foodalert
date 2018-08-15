ba3871bf31964ab001dd1179bc12970f592b0515
Aditya <adikumar@uw.edu>
2018-08-15 11:00:34 -0700

    moved attributes from the data function into props and moved styles into the SFC
    this makes tha app a little bit more idiomatic vue and makes the app more customizable

--- foodalert/static/foodalert/js/components/labelled-textarea.vue
+++ foodalert/static/foodalert/js/components/labelled-textarea.vue
@@ -1,27 +1,29 @@
 <template>
     <div class="form-group row labelled-textarea">
-        <label :for="id"> <b v-if="isOptional">Optional:</b> {{ label_text }}
-        <br />
-        <span :for="id" v-if="moreInfo" class="text-muted"> {{ sub_label}} </span>
+        <label :for="'textarea' + id"> <b v-if="isOptional">
+            Optional:</b> {{ labelText }} <br />
+            <span v-if="subLabel" class="text-muted"> {{ subLabel}} </span>
         </label>
-        <textarea :id="id" class="form-control form-control-lg col-12"> </textarea>
-        <span>{{ num_characters }} characters left</span>
+        <textarea :id="'textarea' + id" class="form-control form-control-lg col-12"> </textarea>
+        <span>{{ numCharacters }} characters left</span>
     </div>
 </template>
 
 <script>
     export default {
+        props: {
+            labelText: String,
+            subLabel: String,
+            isOptional: Boolean,
+        },
         data: function () {
             return {
-                label_text: "test text",
-                sub_label: "test sub",
-                moreInfo: true,
                 id: null,
                 isOptional: true,
-                num_characters: 100,
+                numCharacters: 100,
             }
         },
-        mounted: function () {
+        mounted() {
             this.id = this._uid;
         }
     }
