1f895a041a9e88ebc92321714609c167ee0ffe0a
Noah Eisfelder <neisfelder@me.com>
2018-08-17 11:46:04 -0700

    Updated category heading padding
    Put space below to differentiate from category fields. Removed top padding on Icon to align with title

--- foodalert/static/foodalert/js/components/form-category.vue
+++ foodalert/static/foodalert/js/components/form-category.vue
@@ -1,6 +1,6 @@
 <template>
     <b-container class="form-category my-3" :aria-labelledby="'category-heading-' + id">
-        <h2 class="h5" :id="'category-heading-' + id"><font-awesome-icon class="category-icon pt-1" :icon="iconName" />&nbsp; {{ sectionName  }} </h2>
+        <h2 class="h5 pt-1 pb-5" :id="'category-heading-' + id"><font-awesome-icon class="category-icon" :icon="iconName" />&nbsp; {{ sectionName  }} </h2>
         <slot></slot>
     </b-container>
 </template>
