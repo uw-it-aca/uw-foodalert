91a386be6877ae6422dbd086d8ac2ad31b5ec4eb
Aditya <adikumar@uw.edu>
2018-08-17 11:05:08 -0700

    full event host form preview
    last commit only contained some of the changes

--- foodalert/static/foodalert/js/main.js
+++ foodalert/static/foodalert/js/main.js
@@ -4,18 +4,22 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUtensils, faChevronLeft } from '@fortawesome/free-solid-svg-icons'
+import { faUtensils, faChevronLeft, faCalendar, faClock, faMapMarkerAlt, faClipboardCheck } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faUtensils)
 library.add(faChevronLeft)
+library.add(faCalendar)
+library.add(faClock)
+library.add(faMapMarkerAlt)
+library.add(faClipboardCheck)
 Vue.use(BootstrapVue);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 require('../css/styles.css');
 
-import LabelledTextarea from './components/labelled-textarea.vue';
+import LabelledInput from './components/labelled-input.vue';
 import FormCategory from './components/form-category.vue';
 import AgreePop from './components/agreement-popup.vue';
 
@@ -24,11 +28,16 @@ var app = new Vue({
     el: '#app',
     data: function() {
         return {
+            navVisible: false,
         }
     },
     components: {
-        'labelled-textarea': LabelledTextarea,
+        'labelled-input': LabelledInput,
         'form-category': FormCategory,
         'agreement-popup': AgreePop,
     },
 });
+
+window.onscroll = function () {
+    app.navVisible = window.scrollY;
+};
