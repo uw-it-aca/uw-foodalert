1a904c9667f165d417ffeaf80c7218851d7ae215
Noah Eisfelder <neisfelder@me.com>
2018-08-23 13:52:32 -0700

    Unified styling accross host "Create Notification" pages
    Changes to layout, font weight, text content and other styling elements to make visual style more unified and legible

--- foodalert/static/foodalert/js/components/update-template.vue
+++ foodalert/static/foodalert/js/components/update-template.vue
@@ -1,18 +1,18 @@
 <template>
-    <b-container>
+    <b-container class="px-4 pt-3">
         <p><strong>Thanks for Sharing! Here's what's next:</strong></p>
 
         <p class="mt-4"> Let people know when food runs out </p>
-        <b-btn class="w-100 mb-3" variant="primary" @click="modalShowing = true"> No Food Left </b-btn>
+        <b-btn class="w-100 mb-3 py-2" variant="primary" @click="modalShowing = true"> No Food Left </b-btn>
 
         <labelled-input
-            label-text="Notify people if plans Change"
+            label-text="Notify people if plans change"
             is-optional
             :rows="2"
             example-text="e.g. We moved to HUB 230! Still 4 more sandwiches"
             class="mt-3">
         </labelled-input>
-        <b-btn class="w-100 mb-3"> Send Update </b-btn>
+        <b-btn class="w-100 mb-3 py-2"> Send Update </b-btn>
 
         <b-modal
             v-model="modalShowing"
@@ -20,13 +20,14 @@
             footer-border-variant="0"
             class="text-center"
             footer-class="d-flex flex-column">
-            <p> Are you sure you'd like to notify student's there's <strong> no food left </strong> </p>
+            <p class="h5 pb-3"> Are you sure you'd like to notify student's there's <strong> no food left</strong>? </p>
 
-            <blockquote class="border rounded mb-0">What do blockquotes look like?</blockquote>
+            <blockquote class="border rounded mb-0 mx-3 p-3 text-left bg-light">Update: No Food left! “Hot Indian buffet food leftover
+            from from FIUTs weekly meeting” is all gone.</blockquote>
 
 
             <template slot="modal-footer">
-                <b-button class="mx-auto mb-2" variant="primary"> Send 'No Food Left' </b-button>
+                <b-button class="btn-lg mx-auto mb-2" variant="primary"> Send 'No Food Left' </b-button>
                 <a href="#" @click="modalShowing = false" class="mx-auto"> Cancel </a>
             </template>
         </b-modal>
