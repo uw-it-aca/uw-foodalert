901244de07ed8d37c8c904a840968239007dc517
KeithSRoberts <robertsskeith@gmail.com>
2018-09-13 13:27:50 -0700

    Added tests for posting notifications, added mock post response
    Modified test set up and resources for notification gets

--- foodalert/test/test_notification.py
+++ foodalert/test/test_notification.py
@@ -6,23 +6,33 @@ from foodalert.models import *
 from foodalert.serializers import *
 
 class NotificationTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """
+        Sets up a single mock user that can be used for all tests on
+        notification tests
+        """
+        super(NotificationTest, cls).setUpClass()
+        cls.user = User.objects.create_user(username='testuser',
+                                            email="testuser@test.com",
+                                            password="test")
 
     def setUp(self):
-        # Set up a test user for the notification host
-        user = User.objects.create_user(username='testuser',
-                                        email="testuser@test.com",
-                                        password="test")
         # Set up a test notification with arbitrary field values
         notification = Notification.objects.create(location="UW Campus",
                                                    event="UW Event",
                                                    food_served="Food",
                                                    amount_of_food_left="No Food",
-                                                   host = user,
+                                                   host = self.user,
                                                    host_user_agent="browser")
         self.notification = notification
 
     def tearDown(self):
         self.notification.delete()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.user.delete()
 
     def test_get_notification_list(self):
         """
@@ -37,13 +47,15 @@ class NotificationTest(TestCase):
         # Get all notifications from the notification endpoint
         response = client.get('/notification/')
         # Assert that the response is successful (200 HTTP Response Code)
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
         # Convert response to JSON
         actual_json = response.json()
+        # Set the created time to match as this field is dynamic/based on time
+        expected_json[0]["created_time"] = actual_json[0]["created_time"]
         # Assert that the content is a list with a single notification entry
-        self.assertEquals(len(actual_json), 1)
+        self.assertEqual(len(actual_json), 1)
         # Assert that the json of the notification is correct to the expected
-        self.assertEquals(actual_json[0]['location'], expected_json[0]['location'])
+        self.assertEqual(expected_json[0], actual_json[0])
 
     def test_get_notification_detail_by_id(self):
         """
@@ -59,8 +71,75 @@ class NotificationTest(TestCase):
         url = '/notification/' + str(self.notification.id) + '/'
         response = client.get(url)
         # Assert that the response is successful
-        self.assertEquals(response.status_code, 200)
-
+        self.assertEqual(response.status_code, 200)
+        # Convert response to JSON as the actual json
         actual_json = response.json()
-        self.assertEquals(actual_json['location'], expected_json['location'])
+        # Set the created time to match as this field is dynamic/based on time
+        expected_json["created_time"] = actual_json["created_time"]
+        self.assertEqual(expected_json, actual_json)
 
+    def test_post_valid_notification(self):
+        """
+        Attempts to post a valid notification payload and tests that the
+        request is successful and the response json is matches the request data
+        """
+        # Load mock JSON for a typical notification post response
+        path = '/app/foodalert/test/resources/notification_post_response.json'
+        with open(path) as data_file:
+            expected_json = json.load(data_file)
+        # Test the post with a valid payload (all fields necessary are there)
+        valid_payload = {
+                "location": "UW Campus",
+                "location_details": "North Campus",
+                "event": "UW Event",
+                "end_time": "2018-09-13T19:23:06.508534Z",
+                "food_served": "Food",
+                "host": 1,
+                "amount_of_food_left": "No Food",
+                "bring_container": False,
+                "safe_foods": "",
+                "allergens": "",
+                "host_permit_number": "12345",
+                "host_user_agent": "browser",
+            }
+        client = Client()
+        response = client.post(
+                "/notification/",
+                data=json.dumps(valid_payload),
+                content_type='application/json'
+            )
+        # Assert that the posted notificaiton was sucessfully created
+        self.assertEqual(response.status_code, 201)
+        # Convert the response to JSON as the actual json
+        actual_json = response.json()
+        # Set the created time to match as this field is dynamic/based on time
+        expected_json["created_time"] = actual_json["created_time"]
+        self.assertEqual(expected_json, actual_json)
+
+    def test_post_invalid_notification(self):
+        """
+        Attempts to post an invalid notification payload and tests that the
+        request is unsuccessful
+        """
+        invalid_payload = {
+               "location": "",
+               "location_details": "North Campus",
+               "event": "",
+               "end_time": "",
+               "food_served": "Food",
+               "host": 1,
+               "amount_of_food_left": "No Food",
+               "bring_container": False,
+               "safe_foods": "",
+               "allergens": "",
+               "host_permit_number": "12345",
+               "host_user_agent": "browser",
+           }
+        client = Client()
+        response = client.post(
+                "/notification/",
+                data=json.dumps(invalid_payload),
+                content_type='application/json'
+            )
+		# Assert that the posted notification errors as a bad request
+        self.assertEqual(response.status_code, 400)
