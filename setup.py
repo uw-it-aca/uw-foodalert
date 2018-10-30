5a6d55411d0e84f5b04af18307faa50cddbf669e
Aditya <adikumar@uw.edu>
2018-10-30 12:15:53 -0700

    allowed for any django version < 2
    phonenumber compatibility

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'django==1.11',
+        'django<2.0',
         'django-webpack-loader',
         'UW-Django-SAML2<1.0',
         'psycopg2',
