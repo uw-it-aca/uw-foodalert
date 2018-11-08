ab4a90dedc52caa45094a2dc02f14747a2f60aff
KeithSRoberts <robertsskeith@gmail.com>
2018-11-08 09:57:41 -0800

    Added audit-template snapshot tests, removed items from data in audit component

--- foodalert/static/foodalert/js/components/audit-template.vue
+++ foodalert/static/foodalert/js/components/audit-template.vue
@@ -13,14 +13,9 @@
 </template>
 
 <script>
-    const items = [
-        { dateSent: '10/22/18', timeSent: '4:44PM', endTime: '5:32PM', updateTime: '5:10PM', host: 'netid@uw.edu', permitNumber: '12345678', foodAndEvent: 'A Food and Event', quantity: 'About 8 meals', location: 'HUB 101', contains: 'Dairy, Meat' }
-    ]
-
     export default {
         data() {
             return {
-                items: items
             }
         },
         components: {
