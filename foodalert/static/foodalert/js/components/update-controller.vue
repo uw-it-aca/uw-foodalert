ad2555a24278c5f4d15d82fd735194449f08970e
KeithSRoberts <robertsskeith@gmail.com>
2018-12-21 15:08:01 -0800

    Added validation and navigation to Update form, added post requests from form to update endpoint
    Hooks the client up to post successfully to the update endpoint

--- foodalert/static/foodalert/js/components/update-controller.vue
+++ foodalert/static/foodalert/js/components/update-controller.vue
@@ -1,13 +1,68 @@
 <template>
-    <update-template></update-template>
+    <update-template
+        v-bind:text.sync="form.text"
+        @submitRequest="this.buildRequest"
+        :v="$v"
+        >
+    </update-template>
 </template>
 
 <script type="text/javascript">
     import UpdateTemplate from './update-template.vue';
+    import Cookies from 'js-cookie';
+    import { validationMixin } from "vuelidate"
+    import { required, maxLength } from "vuelidate/lib/validators"
+    const axios = require('axios');
+
     export default {
         components: {
             'update-template': UpdateTemplate,
-        }
+        },
+        mounted() {
+            this.uid = this._uid;
+            this.notificationID = parseInt(this.$route.query.notificationID);
+        },
+        data() {
+            return {
+                uid: 0,
+                notificationID: 0,
+                form: {
+                    text: ""
+                },
+            }
+        },
+        methods: {
+            setText(payload) {
+                this.text = payload;
+            },
+            buildRequest() {
+                var data = {
+                    "text": this.form.text,
+                    "parent_notification": this.notificationID
+                };
+                var csrftoken = Cookies.get('csrftoken');
+                var headers = {
+                    'Content-Type': 'application/json',
+                    'X-CSRFToken': csrftoken,
+                }
+                axios.post('/updates/', data, {"headers": headers})
+                    .then(function(response) {
+                        console.log(response);
+                    }.bind(this))
+                    .catch(function (error) {
+                        console.log("There was an error processing the request");
+                        console.log(error);
+                    })
+            }
+        },
+        validations: {
+            form: {
+                text: {
+                    required,
+                    maxLength: maxLength(100)
+                }
+            }
+        },
     }   
 </script>
 
