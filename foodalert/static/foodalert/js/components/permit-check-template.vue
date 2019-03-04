5587b204db6b0392eebe3520ce6c33cf33b7687f
Timothy Sun <tsun1@uw.edu>
2019-03-04 10:56:42 -0800

    Permit check component
    New user flow has permit check not as a modal but as a separate page

--- foodalert/static/foodalert/js/components/permit-check-template.vue
+++ foodalert/static/foodalert/js/components/permit-check-template.vue
@@ -1,16 +1,56 @@
 <template>
-  <b-container fluid class="p-0">
-    <h1>Hello</h1>
+  <b-container>
+    <h1>Tell us about your event</h1>
+    <b-form @submit="proceed">
+      <form-category>
+        <label for="eventName">
+          Event Name
+        </label>
+        <b-form-input
+          type="text" 
+          placeholder="FIUTS weekly club meeting"
+          id="eventName">
+        </b-form-input>
+        <b-form-group label="Individual radios" v-model="this.selected">
+          <b-form-radio value="Yes" name="yesNo">Yes</b-form-radio>
+          <b-form-radio value="No" name="yesNo">No</b-form-radio>
+        </b-form-group>
+         <b-button type="submit">Submit</b-button>
+      </form-category>
+    </b-form>
   </b-container>
-
-
-
 </template>
 
 <script>
-export default {
-}
+    import FormCategory from './form-category.vue';
+    import LabelledInput from './labelled-input.vue';
+    import PopupContainer from "./popup-container.vue";
+
+    export default {
+        data(){
+          return{
+            selected: ""
+          }
+        },
+        methods: {
+            updateValue(context) {
+                this.$emit('updateState',  context);
+            },
+            setValue(context, value) {
+                this.$emit('setState', context, value);
+            },
+            proceed() {
+              this.$router.push({ name: 'form'});
+            }
+        },
+        components: {
+            'form-category': FormCategory,
+            'labelled-input': LabelledInput,
+            'popup-container': PopupContainer,
+        }
+    }
 </script>
+
 
 <style lang="css" scoped>
 </style>
