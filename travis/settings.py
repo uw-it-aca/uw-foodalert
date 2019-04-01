8ad62b5e0f3844c85fd384372fb55386af08ba2d
KeithSRoberts <robertsskeith@gmail.com>
2019-04-01 11:37:41 -0700

    Added seperate django-project for travis ci, travis.yml uses new travis settings

---
+++ travis/settings.py
@@ -0,0 +1,181 @@
+import os
+import sys
+from django.urls import reverse_lazy
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+SECRET_KEY = os.getenv('DJANGO_SECRET', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_user_agents',
+    'webpack_loader',
+    'foodalert',
+    'rest_framework',
+    'phonenumber_field',
+]
+
+ALLOWED_HOSTS = []
+`
+MIDDLEWARE = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.PersistentRemoteUserMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_user_agents.middleware.UserAgentMiddleware',
+]
+
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.RemoteUserBackend',
+]
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'America/Los_Angeles'
+
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+ROOT_URLCONF = 'travis.urls'
+WSGI_APPLICATION = 'travis.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': ('django.contrib.auth.password_validation'
+                 '.UserAttributeSimilarityValidator'),
+    },
+    {
+        'NAME': ('django.contrib.auth.password_validation'
+                 '.MinimumLengthValidator'),
+    },
+    {
+        'NAME': ('django.contrib.auth.password_validation'
+                 '.CommonPasswordValidator'),
+    },
+    {
+        'NAME': ('django.contrib.auth.password_validation'
+                 '.NumericPasswordValidator'),
+    },
+]
+
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
+
+COMPRESS_ENABLED = False
+COMPRESS_ROOT = '/static/'
+STATIC_ROOT = '/static/'
+STATIC_URL = '/static/'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'debug':  True,
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        }
+    }
+]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'formatters': {
+    },
+    'handlers': {
+        'stdout': {
+            'level':'INFO',
+            'class':'logging.StreamHandler',
+            'strm': sys.stdout
+        },
+        'stderr': {
+            'level':'ERROR',
+            'class':'logging.StreamHandler',
+            'strm': sys.stderr
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['stdout'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        '': {
+            'handlers': ['stderr'],
+            'level': 'ERROR',
+            'propagate': False,
+        }
+    }
+}
+
+INSTALLED_APPS += ['uw_saml']
+
+MOCK_SAML_ATTRIBUTES = {
+    'uwnetid': ['javerage'],
+    'affiliations': ['student', 'member', 'alum', 'staff', 'employee'],
+    'eppn': ['javerage@washington.edu'],
+    'scopedAffiliations': ['student@washington.edu', 'member@washington.edu'],
+    'isMemberOf': ['u_test_group', 'u_test_another_group',
+                   'u_astratest_myuw_test-support-admin'],
+}
+
+LOGIN_URL = reverse_lazy('saml_login')
+LOGOUT_URL = reverse_lazy('saml_logout')
+
+REMOTE_USER_FORMAT = 'uwnetid'
+MOCK_SAML_ATTRIBUTES['isMemberOf'] = ['u_test_host', 'u_test_admin']
+
+FOODALERT_AUTHZ_GROUPS = {
+    'create': 'u_test_host',
+    'audit': 'u_test_admin'
+}
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'CACHE': not DEBUG,
+        'BUNDLE_DIR_NAME': 'foodalert/bundles/',  # must end with slash
+        'STATS_FILE': os.path.join(BASE_DIR, 'docker', 'webpack-stats.json'),
+        'POLL_INTERVAL': 0.1,
+        'TIMEOUT': None,
+        'IGNORE': [r'.+\.hot-update.js', r'.+\.map']
+    }
+}
+EMAIL_BACKEND = "saferecipient.EmailBackend"
