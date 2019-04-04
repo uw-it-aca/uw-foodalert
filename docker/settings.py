a50ee6f3fd1a4c65fa4dfffd560da8bbeafbab96
Aditya <adikumar@uw.edu>
2019-04-04 14:33:30 -0700

    Moved webpack-stats.json to project directory inside docker container. docker directory is now extraneous after everything is copied out of it

--- docker/settings.py
+++ docker/settings.py
@@ -118,7 +118,7 @@ WEBPACK_LOADER = {
     'DEFAULT': {
         'CACHE': not DEBUG,
         'BUNDLE_DIR_NAME': 'foodalert/bundles/',  # must end with slash
-        'STATS_FILE': os.path.join(BASE_DIR, 'docker', 'webpack-stats.json'),
+        'STATS_FILE': os.path.join(BASE_DIR, 'project', 'webpack-stats.json'),
         'POLL_INTERVAL': 0.1,
         'TIMEOUT': None,
         'IGNORE': [r'.+\.hot-update.js', r'.+\.map']
