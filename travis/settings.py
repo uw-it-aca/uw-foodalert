ec35a2cfca39c7a88a50016a4dac1d2004b4cc4c
KeithSRoberts <robertsskeith@gmail.com>
2019-04-15 12:11:12 -0700

    Finished removing extra variables from travis settings file

--- travis/settings.py
+++ travis/settings.py
@@ -5,46 +5,29 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = os.getenv('DJANGO_SECRET', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django_user_agents',
-    'webpack_loader',
+    'django.contrib.sites',
     'foodalert',
     'rest_framework',
-    'phonenumber_field',
     'dbmail',
-    'premailer',
-    'django.contrib.sites',
 ]
 
 SITE_ID = 1
 
-ALLOWED_HOSTS = []
-
 MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.PersistentRemoteUserMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_user_agents.middleware.UserAgentMiddleware',
 ]
 
-USE_I18N = True
-USE_L10N = True
 USE_TZ = True
 
 ROOT_URLCONF = 'travis.urls'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'travis_ci_test',
         'USER': 'travis',
         'PASSWORD': '',
@@ -54,44 +37,25 @@ DATABASES = {
 }
 
 STATIC_ROOT = '/static/'
-STATIC_URL = '/static/'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'debug':  True,
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        }
     }
 ]
 
-MOCK_SAML_ATTRIBUTES = {
-    'uwnetid': ['javerage'],
-}
-
-# AWS Config for Boto3
 AWS_ACCESS_KEY_ID = ''
 AWS_SECRET_ACCESS_KEY = ''
 
-# Twilio Configuration Settings
-# Note: These must be set (to anything) for tests to work
 TWILIO_ACCOUNT_SID = "XXX"
 TWILIO_AUTH_TOKEN = "XXX"
 TWILIO_NOTIFY_SERVICE_ID = "XXX"
 TWILIO_FROM = ''
 
+MOCK_SAML_ATTRIBUTES = {}
 MOCK_SAML_ATTRIBUTES['isMemberOf'] = ['u_test_host', 'u_test_admin']
 
 FOODALERT_AUTHZ_GROUPS = {
     'create': 'u_test_host',
     'audit': 'u_test_admin'
 }
-
-EMAIL_BACKEND = "saferecipient.EmailBackend"
