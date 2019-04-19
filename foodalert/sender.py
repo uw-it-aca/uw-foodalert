60beb364ecf783db996fd84ff104ab1bf07e4a6a
KeithSRoberts <robertsskeith@gmail.com>
2019-04-19 10:48:00 -0700

    Removed print statements used for debugging

--- foodalert/sender.py
+++ foodalert/sender.py
@@ -6,7 +6,6 @@ from django.conf import settings
 class Sender:
     def send_email(body, recipients, time):
         if body[:6] == 'Update':
-            print('making update with ' + time)
             MailTemplate.objects.create(
                 name="Food Alert Update",
                 subject="A Hungry Husky Event has posted an update",
@@ -15,7 +14,6 @@ class Sender:
                 is_html=False,
             )
         else:
-            print('making notif with ' + time)
             MailTemplate.objects.create(
                 name="Food Alert Notification",
                 subject="New Hungry Husky Event is Open",
