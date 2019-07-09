a9fa9f5640e57e2121789791fc8adfd2f34de174
Sarthak Singh <ss269@uw.edu>
2019-07-09 13:33:42 -0700

    more maintance work

--- foodalert/static/foodalert/js/signup.js
+++ foodalert/static/foodalert/js/signup.js
@@ -1,20 +1,18 @@
-import SignupTemplate from './components/signup-template.vue';
-import SignupController from './components/signup-controller.vue';
-import SubscribedTemplate from './components/subscribed-template.vue';
-import SubscribedController from './components/subscribed-controller.vue';
+//import SignupController from './components/signup-controller.vue';
+//import SubscribedController from './components/subscribed-controller.vue';
 import StudentWelcome from './pages/student/welcome.vue';
 import ResponsibitiesPage from './pages/student/responsibilities-page.vue';
 import NotificationPage from './pages/student/notification-page.vue';
 
 window.vueData.routes[0].children = window.vueData.routes[0].children.concat([
-    { path: "signup", component: SignupController },
-    { path: "subscribed", component: SubscribedController },
+    //{ path: "signup", component: SignupController },
+    //{ path: "subscribed", component: SubscribedController },
     { path: "student-welcome", component: StudentWelcome },
 ]);
 
 let rootPath = window.vueData.routes.find(obj => obj.path === "/");
-rootPath.children.find(obj => obj.path === "s/signup").component = SignupController;
-rootPath.children.find(obj => obj.path === "s/subscribed").component = SubscribedController;
+//rootPath.children.find(obj => obj.path === "s/signup").component = SignupController;
+//rootPath.children.find(obj => obj.path === "s/subscribed").component = SubscribedController;
 rootPath.children.find(obj => obj.path === "s/welcome").component = StudentWelcome;
 rootPath.children.find(obj => obj.path === "s/responsibilities").component = ResponsibitiesPage;
 rootPath.children.find(obj => obj.path === "s/notifications").component = NotificationPage;
