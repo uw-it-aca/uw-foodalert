4d180fa87a7e132d6b7b3842944653f2950ec039
erinRochfort99 <erochfor@uw.edu>
2019-07-12 10:41:15 -0700

    update to checkboxes

--- foodalert/static/foodalert/js/pages/student/responsibilities-page.vue
+++ foodalert/static/foodalert/js/pages/student/responsibilities-page.vue
@@ -8,16 +8,18 @@
                 We'll need you to agree to the following terms.
             </p>
             <b-form @submit="getNextPage()" ref="resForm">
-                <custom-checkbox
-                    :v_model.sync="selected"
+                <b-form-checkbox
+                    v-model="selected"
+                    name="cond1"
                     value="cond1" required>
-                    We cannot confirm all potential allergy ingredients.
-                </custom-checkbox>
-                <custom-checkbox
-                    :v_model.sync="selected"
-                    c_value="cond2" required>
-                    Placeholder for terms and service
-                </custom-checkbox>
+                    <span>We cannot confirm all potential allergy ingredients.</span>
+                </b-form-checkbox>
+                <b-form-checkbox
+                    v-model="selected"
+                    name="cond2"
+                    value="cond2" required>
+                    <span>Placeholder for terms and service</span>
+                </b-form-checkbox>
                 <div class="mt-4">
                     <b-row align-h="between">
                         <b-col md="4" lg="3" order-md="2">
@@ -35,13 +37,11 @@
 <script type="text/javascript">
     import GenericPage from "../../components/generic-page.vue";
     import CollapseTextBox from "../../components/collapse-text-box.vue";
-    import CustomCheckbox from "../../components/custom-checkbox.vue";
 
     export default {
         components:{
             "generic-page": GenericPage,
             "collapse-text-box": CollapseTextBox,
-            "custom-checkbox": CustomCheckbox,
         },
         props: {
             bid: String,
