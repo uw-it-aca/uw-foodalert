b9baa78e6793daa6b65226e4f6922740569a8cad
Sarthak Singh <ss269@uw.edu>
2019-07-15 11:03:50 -0700

    Improved form test

--- foodalert/static/foodalert/js/__tests__/host-form-page.spec.js
+++ foodalert/static/foodalert/js/__tests__/host-form-page.spec.js
@@ -6,25 +6,37 @@ Vue.use(BootstrapVue);
 import {mount} from '@vue/test-utils';
 import FormPage from '../pages/host/form-page.vue';
 
-jest.mock('axios', () => ({
-	get: jest.fn((data) => Promise.resolve({data: []})),
-	post: jest.fn(() => Promise.resolve())
-}))
-
 import axios from 'axios'
+
+jest.mock('axios', () => ({
+	get: jest.fn(),
+	post: jest.fn()
+}))
 
 const $routes = {
     push: jest.fn()
 }
 
 describe('Food Categories Page', function () {
-        test('renders correctly', function () {
-            var wrapper = mount(FormPage, {
-            });
-            expect(wrapper.element).toMatchSnapshot();
-        });
+	test('renders correctly', function () {
+		axios.get.mockImplementation(() => Promise.resolve({data: []}));
+		axios.post.mockImplementation(() => Promise.resolve({data: [{name: "allergne_1"}, {name: "allergen_2"}]}));
+		var wrapper = mount(FormPage, {
+		});
+		expect(wrapper.element).toMatchSnapshot();
+	});
+
+	test('redirect to update', function () {
+		axios.get.mockImplementation(() => Promise.resolve({data: [{ended: true}]}));
+		axios.post.mockImplementation(() => Promise.resolve({data: []}));
+		var wrapper = mount(FormPage, {
+		});
+		expect(wrapper.element).toMatchSnapshot();
+	});
 
 	test('form submit', function () {
+		axios.get.mockImplementation(() => Promise.resolve({data: []}));
+		axios.post.mockImplementation(() => Promise.resolve({data: []}));
 	    var wrapper = mount(FormPage, {
 	        $routes
 	    });
@@ -37,12 +49,17 @@ describe('Food Categories Page', function () {
 	    var textarea = wrapper.find('textarea')
 	    textarea.setValue("some random value")
 
+		var form = wrapper.find("form")
+		form.trigger("submit")
+
 	    var buttons = wrapper.findAll('button')
-	    buttons.at(0).trigger('click')
-	    buttons.at(4).trigger('click')
+	    // buttons.at(0).trigger('click')
+		buttons.at(4).trigger('click.native')
 	});
 
 	test('reset test', function () {
+		axios.get.mockImplementation(() => Promise.resolve({data: []}));
+		axios.post.mockImplementation(() => Promise.resolve({data: []}));
 	    var wrapper = mount(FormPage, {
 	        $routes
 	    });
