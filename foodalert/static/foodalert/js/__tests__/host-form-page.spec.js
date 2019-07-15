2f8f17746208ff668ff75073a3cc0a18d68752b0
Sarthak Singh <ss269@uw.edu>
2019-07-15 01:59:09 -0700

    Improved host-form-page.spec.js

--- foodalert/static/foodalert/js/__tests__/host-form-page.spec.js
+++ foodalert/static/foodalert/js/__tests__/host-form-page.spec.js
@@ -6,10 +6,48 @@ Vue.use(BootstrapVue);
 import {mount} from '@vue/test-utils';
 import FormPage from '../pages/host/form-page.vue';
 
+jest.mock('axios', () => ({
+	get: jest.fn((data) => Promise.resolve({data: []})),
+	post: jest.fn(() => Promise.resolve())
+}))
+
+import axios from 'axios'
+
+const $routes = {
+    push: jest.fn()
+}
+
 describe('Food Categories Page', function () {
         test('renders correctly', function () {
             var wrapper = mount(FormPage, {
             });
             expect(wrapper.element).toMatchSnapshot();
         });
+
+	test('form submit', function () {
+	    var wrapper = mount(FormPage, {
+	        $routes
+	    });
+	    var inputs = wrapper.findAll('input')
+	    inputs.at(0).setValue("some random value")
+	    inputs.at(1).setValue("some random value")
+	    inputs.at(2).setValue("12:00 AM")
+	    inputs.at(3).setValue("some random value")
+
+	    var textarea = wrapper.find('textarea')
+	    textarea.setValue("some random value")
+
+	    var buttons = wrapper.findAll('button')
+	    buttons.at(0).trigger('click')
+	    buttons.at(4).trigger('click')
+	});
+
+	test('reset test', function () {
+	    var wrapper = mount(FormPage, {
+	        $routes
+	    });
+
+	    var buttons = wrapper.findAll('button')
+	    buttons.at(1).trigger('click')
+	});
 })
