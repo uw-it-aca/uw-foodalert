918227f240b56bd2301e28cd3fbf8f19fcca7f77
Sarthak Singh <ss269@uw.edu>
2019-08-13 15:23:56 -0700

    fixed unrecoverable

--- foodalert/static/foodalert/js/pages/unrecoverable-page.vue
+++ foodalert/static/foodalert/js/pages/unrecoverable-page.vue
@@ -1,30 +1,37 @@
 <template>
-    <generic-page>
-        <template #heading>
-            <p class="foodalert-error-heading">{{errorHeading}}</p>
-        </template>
-        <template #body>
-            <p>
-                {{errorMessage}}
-            </p>
-            <p>
-                Error code: {{errorCode}}
-            </p>
-            <p>
-                Please <a href="mailto:help@uw.edu?subject=Hungry Husky support">contact support</a> to get help.
-            </p>
-        </template>
-        <template #navigation>
-            <div style="margin-top: 32px">
-               <b-row align-h="between">
-                 <b-col md="4" lg="3" order-md="2">
-                   <b-button class="mb-3" type="submit" block size="lg" variant="primary" @click="getNextPage()">Try Again</b-button>
-                 </b-col>
-                 <b-col md="5" lg="4" order-md="1"></b-col>
-               </b-row>
-            </div>
-        </template>
-    </generic-page>
+  <generic-page>
+    <template #heading>
+      <p class="foodalert-error-heading">{{errorHeading}}</p>
+    </template>
+    <template #body>
+      <p>
+        {{errorMessage}}
+      </p>
+      <p>
+        Error code: {{errorCode}}
+      </p>
+      <p>
+        Please
+        <a href="mailto:help@uw.edu?subject=Hungry Husky support">
+          contact support
+        </a>
+        to get help.
+      </p>
+    </template>
+    <template #navigation>
+      <div style="margin-top: 32px">
+        <b-row align-h="between">
+          <b-col md="4" lg="3" order-md="2">
+            <b-button class="mb-3" type="submit" block size="lg"
+              variant="primary" @click="getNextPage()">
+              Try Again
+            </b-button>
+          </b-col>
+          <b-col md="5" lg="4" order-md="1"></b-col>
+        </b-row>
+      </div>
+    </template>
+  </generic-page>
 </template>
 
 <script type="text/javascript">
