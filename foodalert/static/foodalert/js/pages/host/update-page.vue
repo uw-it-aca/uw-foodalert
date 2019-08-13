054df7a3c1b5be081951758c72759aeb89cd4c6c
erinRochfort99 <erochfor@uw.edu>
2019-08-13 15:36:03 -0700

    style changes to update page

--- foodalert/static/foodalert/js/pages/host/update-page.vue
+++ foodalert/static/foodalert/js/pages/host/update-page.vue
@@ -1,64 +1,86 @@
 <template>
-    <generic-page :startWithNotification="privNotifText != undefined" ref="notifBox">
-        <template #notification>
-            {{privNotifText}}
-        </template>
-        <template #heading>
-            Compose update
-        </template>
-        <template #body>
-            <b-modal id="submitconfirmation" title="Confirmation" ok-title="Send" cancel-variant="outline-secondary" @ok="sendUpdate()">
-                <p>
-                    We will send your update to Hungry Husky Subscribers.
-                </p>
-                <preview-box>
-                    Update:
-                    <span v-if="selected == 'noFoodUpdate'"> No food left at {{state.location}} </span>
-                    <span v-else-if="selected == 'otherUpdate'">
-                        <span v-if="otherText == ''"> We've moved to HUB 120 </span>
-                        <span v-else> {{otherText}} </span>
-                    </span>.
-                    Re: {{state.food.served}} leftover from {{state.event}}...
-                </preview-box>
-            </b-modal>
-            <h2 class="h2">Don't leave people stranded!</h2>
-            <p class="p pb-3">
-                When the food is all gone, please return here to send an update. This will prevent people from making unnecessary trips.
-            </p>
-            <b-form>
-                <b-form-radio-group stacked v-model="selected">
-                    <b-form-radio value="noFoodUpdate">
-                        <span>No food left</span>
-                    </b-form-radio>
-                    <b-form-radio id="otherRadio" value="otherUpdate" class="mt-1">
-                        <span>
-                            Other message
-                            <b-form-input id="other-message" aria-describedby="Other message for the subs"
-                            required placeholder="We've moved to HUB 120" class="mb-3 standard-placeholder" v-model="otherText">
-                            </b-form-input>
-                        </span>
-                    </b-form-radio>
-                </b-form-radio-group>
-            </b-form>
-            <h2 class="h2">Preview</h2>
-            <preview-box>
-                Update:
-                <span v-if="selected == 'noFoodUpdate'"> No food left at {{state.location}} </span>
-                <span v-else-if="selected == 'otherUpdate'">
-                    <span v-if="otherText == ''"> We've moved to HUB 120 </span>
-                    <span v-else> {{otherText}}</span>
-                </span>.
-                Re: {{state.food.served}} leftover from {{state.event}}...
-            </preview-box>
-        </template>
-        <template #navigation>
-            <div class="mt-5">
-                <b-row align-h="between">
-                    <b-col md="5" lg="4" order-md="2"><b-button class="mb-3" type="submit" block variant="primary" style="white-space: nowrap;" size="lg" @click="$bvModal.show('submitconfirmation')" :disabled="(otherText == '') && (selected != 'noFoodUpdate')">Send Update</b-button>
-                    </b-col>
-                </b-row>
-            </div>
-        </template>
+    <generic-page
+      :startWithNotification="privNotifText != undefined"
+      ref="notifBox">
+      <template #notification>
+          {{privNotifText}}
+      </template>
+      <template #heading>
+          Compose update
+      </template>
+      <template #body>
+          <b-modal id="submitconfirmation" title="Confirmation"
+            ok-title="Send" cancel-variant="outline-secondary"
+            @ok="sendUpdate()">
+              <p>
+                We will send your update to Hungry Husky Subscribers.
+              </p>
+              <preview-box>
+                  Update:
+                  <span v-if="selected == 'noFoodUpdate'">
+                    No food left at {{state.location}}
+                  </span>
+                  <span v-else-if="selected == 'otherUpdate'">
+                      <span v-if="otherText == ''">
+                        We've moved to HUB 120
+                      </span>
+                      <span v-else> {{otherText}} </span>
+                  </span>.
+                  Re: {{state.food.served}} leftover from {{state.event}}...
+              </preview-box>
+          </b-modal>
+          <h2 class="h2">Don't leave people stranded!</h2>
+          <p class="p pb-3">
+            When the food is all gone, please return here to send an update.
+            This will prevent people from making unnecessary trips.
+          </p>
+          <b-form>
+            <b-form-radio-group stacked v-model="selected">
+              <b-form-radio value="noFoodUpdate">
+                  <span>No food left</span>
+              </b-form-radio>
+              <b-form-radio id="otherRadio" value="otherUpdate" class="mt-1">
+                  <span>
+                      Other message
+                      <b-form-input
+                        id="other-message"
+                        aria-describedby="Other message for the subs"
+                        required placeholder="We've moved to HUB 120"
+                        class="mb-3 standard-placeholder" v-model="otherText">
+                      </b-form-input>
+                  </span>
+              </b-form-radio>
+            </b-form-radio-group>
+          </b-form>
+          <h2 class="h2">Preview</h2>
+          <preview-box>
+              Update:
+              <span v-if="selected == 'noFoodUpdate'">
+                No food left at {{state.location}}
+              </span>
+              <span v-else-if="selected == 'otherUpdate'">
+                  <span v-if="otherText == ''"> We've moved to HUB 120 </span>
+                  <span v-else> {{otherText}}</span>
+              </span>.
+              Re: {{state.food.served}} leftover from {{state.event}}...
+          </preview-box>
+      </template>
+      <template #navigation>
+          <div class="mt-5">
+              <b-row align-h="between">
+                  <b-col md="5" lg="4" order-md="2">
+                    <b-button class="mb-3" type="submit"
+                      block variant="primary" style="white-space: nowrap;"
+                      size="lg"
+                      @click="$bvModal.show('submitconfirmation')"
+                      :disabled="(otherText == '') &&
+                      (selected != 'noFoodUpdate')">
+                        Send Update
+                    </b-button>
+                  </b-col>
+              </b-row>
+          </div>
+      </template>
     </generic-page>
 </template>
 
@@ -102,11 +124,13 @@ export default {
           if (data.length === 0) {
             this.$router.push({name: 'h-welcomes'});
           } else {
-            axios.get('/notification/' + data[0]['id'] + '/', {'headers': headers}).
-                then((response) => {
+            axios.get('/notification/' + data[0]['id'] + '/',
+                {'headers': headers})
+                .then((response) => {
                   console.log(response.data);
                   this.state = response.data;
-                }).catch((error) => this.showErrorPage(error.response, 'h-update'));
+                }).catch((error) =>
+                  this.showErrorPage(error.response, 'h-update'));
           }
         })
         .catch((error) => this.showErrorPage(error.response, 'h-update'));
@@ -114,13 +138,14 @@ export default {
   methods: {
     sendUpdate() {
       if (this.selected == 'noFoodUpdate') {
-        var data = {
-          'text': 'No Food left! The event: ' + this.state.event + ' has ended and is no longer serving food',
+        const data = {
+          'text': 'No Food left! The event: ' + this.state.event +
+                  ' has ended and is no longer serving food',
           'parent_notification_id': this.state.id,
           'ended': true,
         };
-        var csrftoken = Cookies.get('csrftoken');
-        var headers = {
+        const csrftoken = Cookies.get('csrftoken');
+        const headers = {
           'Content-Type': 'application/json',
           'X-CSRFToken': csrftoken,
         };
@@ -132,12 +157,12 @@ export default {
             }.bind(this))
             .catch((error) => this.showErrorPage(error.response, 'h-update'));
       } else {
-        var data = {
+        const data = {
           'text': this.otherText,
           'parent_notification_id': this.state.id,
         };
-        var csrftoken = Cookies.get('csrftoken');
-        var headers = {
+        const csrftoken = Cookies.get('csrftoken');
+        const headers = {
           'Content-Type': 'application/json',
           'X-CSRFToken': csrftoken,
         };
