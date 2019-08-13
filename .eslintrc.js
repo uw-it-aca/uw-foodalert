17457daa38ec9806461086cd9fa2661abc9a3036
Sarthak Singh <ss269@uw.edu>
2019-08-13 13:38:53 -0700

    Changed linting standards

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,22 @@ module.exports = {
   'env': {
     'browser': true,
     'commonjs': true,
-    'es6': true
+    'es6': true,
   },
   'extends': [
-    'eslint:recommended',
     'plugin:vue/essential',
+    'google',
   ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018,
+  },
   'plugins': [
     'vue',
   ],
   'rules': {
-    "semi": [2, "always"],
-    "vue/max-attributes-per-line": "off",
-    "vue/script-indent" : ["error", 2, { "baseIndent": 1 }]
   },
 };
