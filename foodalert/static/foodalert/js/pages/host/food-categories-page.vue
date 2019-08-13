5ad8c304b79bb680acfac461ed6a9b7144b9e8ff
erinRochfort99 <erochfor@uw.edu>
2019-08-13 15:14:21 -0700

    food-categories style changes

--- foodalert/static/foodalert/js/pages/host/food-categories-page.vue
+++ foodalert/static/foodalert/js/pages/host/food-categories-page.vue
@@ -5,7 +5,8 @@
         </template>
         <template #body>
             <p>
-                The following will help determine whether your food can be shared with the public.
+              The following will help determine whether your food can be
+              shared with the public.
             </p>
             <b-form-group>
                 <b-form-checkbox
@@ -14,11 +15,14 @@
                     @click.native="removeInput('none')">
                     <span>
                         My food is non-perishable.
-                        <b-link herf="#" v-b-toggle.non-perishable> Examples</b-link>
+                        <b-link herf="#" v-b-toggle.non-perishable>
+                          Examples
+                        </b-link>
                     </span>
                 </b-form-checkbox>
                 <collapse-text-box bid="non-perishable">
-                Candy, beverages (pasteurized, canned, or bottled), chips, dips.
+                  Candy, beverages (pasteurized, canned, or bottled),
+                  chips, dips.
                 </collapse-text-box>
                 <b-form-checkbox
                     v-model="selected"
@@ -27,24 +31,29 @@
                     @click.native="['none', 'at-home'].forEach(removeInput)">
                     <span>
                         My food was commercially pre-packaged.
-                        <b-link herf="#" v-b-toggle.pre-packaged> Examples</b-link>
+                        <b-link herf="#" v-b-toggle.pre-packaged>
+                          Examples
+                        </b-link>
                     </span>
                 </b-form-checkbox>
                 <collapse-text-box bid="pre-packaged">
-                    Wrapped or boxed baked goods (cakes, pies), chips, store-bought ice cream.
+                    Wrapped or boxed baked goods (cakes, pies),
+                    chips, store-bought ice cream.
                 </collapse-text-box>
                 <b-form-checkbox
                     v-model="selected"
                     class="mt-3"
                     value="at-home"
-                    @click.native="['none', 'pre-packaged', 'non-perishable'].forEach(removeInput)">
+                    @click.native="['none', 'pre-packaged', 'non-perishable']
+                      .forEach(removeInput)">
                     My food was prepared at home.
                 </b-form-checkbox>
                 <b-form-checkbox
                     v-model="selected"
                     class="mt-3"
                     value="none"
-                    @click.native="['non-perishable', 'pre-packaged', 'at-home'].forEach(removeInput)">
+                    @click.native="['non-perishable', 'pre-packaged',
+                      'at-home'].forEach(removeInput)">
                     <span>
                         None of the above.
                     </span>
@@ -55,10 +64,24 @@
             <div class="mt-5">
                <b-row align-h="between">
                  <b-col md="4" lg="3" order-md="2">
-                   <b-button class="mb-3" type="submit" block size="lg" variant="primary" @click="getNextPage()" :disabled="selected.length == 0">Continue</b-button>
+                   <b-button
+                    class="mb-3"
+                    type="submit"
+                    block size="lg"
+                    variant="primary"
+                    @click="getNextPage()"
+                    :disabled="selected.length == 0">
+                      Continue
+                   </b-button>
                  </b-col>
                  <b-col md="4" lg="3" order-md="1">
-                   <b-button class="hh-back-button" type="submit" block size="lg" variant="outline-secondary" @click="getBackPage()">Back</b-button>
+                    <b-button
+                      class="hh-back-button"
+                      type="submit" block size="lg"
+                      variant="outline-secondary"
+                      @click="getBackPage()">
+                        Back
+                    </b-button>
                  </b-col>
                </b-row>
             </div>
@@ -81,9 +104,11 @@ export default {
   },
   methods: {
     getNextPage() {
-      if (this.selected.includes('non-perishable') || this.selected.includes('pre-packaged')) {
+      if (this.selected.includes('non-perishable') ||
+          this.selected.includes('pre-packaged')) {
         this.$router.push({name: 'h-need-permit'});
-      } else if (this.selected.includes('none') || this.selected.includes('atHome')) {
+      } else if (this.selected.includes('none') ||
+                 this.selected.includes('atHome')) {
         this.$router.push({name: 'h-close'});
       }
     },
@@ -105,7 +130,9 @@ export default {
     axios.get('/notification/?host_netid=' + this.netID).then((result) => {
       result.data = result.data.filter((d)=>!d.ended);
       if (result.data.length) {
-        this.$router.push({name: 'h-update', params: {notificationText: 'You already have an event running.'}});
+        this.$router.push({
+          name: 'h-update',
+          params: {notificationText: 'You already have an event running.'}});
       }
     }).catch((error) => this.showErrorPage(error.response, 'h-categories'));
   },
