13148765786ef8e863c6a7b38c2e9d205f2ef8c0
erinRochfort99 <erochfor@uw.edu>
2019-08-13 17:23:30 -0700

    added aria labels to links

--- foodalert/static/foodalert/js/pages/host/responsibilities-page.vue
+++ foodalert/static/foodalert/js/pages/host/responsibilities-page.vue
@@ -9,40 +9,43 @@
               to agree to all of the following requirements.
             </p>
             <b-form @submit="getNextPage()" ref="resForm">
-                <b-form-checkbox
-                    v-model="selected"
-                    value="cond1">
-                    <span>
-                        My office is responsible for the safety of this food.
-                        <b-link herf="#" v-b-toggle.cond1-info>
-                          Learn more
-                        </b-link>
-                    </span>
-                </b-form-checkbox>
-                <collapse-text-box bid="cond1-info">
-                  You are responsible for food safety under the approval of
-                  the Temporary Food Permit. This same level of responsibility
-                  applies when using the Hungry Husky App.
-                </collapse-text-box>
-                <b-form-checkbox
-                    v-model="selected"
-                    class="mt-2"
-                    value="cond2">
-                    <span>
-                      Potentially hazardous food must be kept at proper
-                      temperatures and have been sitting at room temperature
-                      for no more than <strong>four hours</strong>.
-                      <b-link herf="#" v-b-toggle.cond2-info>
-                        Learn more
-                      </b-link>
-                    </span>
-                </b-form-checkbox>
-                <collapse-text-box bid="cond2-info">
-                    Information is missing
-                </collapse-text-box>
-                <div class="mt-5">
-                    <b-row align-h="between">
-                        <b-col md="4" lg="3" order-md="2">
+              <b-form-checkbox
+                v-model="selected"
+                class="mt-2"
+                value="cond1">
+                <span>
+                  My office is responsible for the safety of this food.
+                  <b-link herf="#" v-b-toggle.cond1-info
+                    aria-label="Learn more about safety responsibilities">
+                    Learn more
+                  </b-link>
+                </span>
+              </b-form-checkbox>
+              <collapse-text-box bid="cond1-info">
+                You are responsible for food safety under the approval of
+                the Temporary Food Permit. This same level of responsibility
+                applies when using the Hungry Husky App.
+              </collapse-text-box>
+              <b-form-checkbox
+                v-model="selected"
+                class="mt-2"
+                value="cond2">
+                <span>
+                  Potentially hazardous food must be kept at proper
+                  temperatures and have been sitting at room temperature
+                  for no more than <strong>four hours</strong>.
+                  <b-link herf="#" v-b-toggle.cond2-info
+                    aria-label="Learn more about potentially hazardous food">
+                    Learn more
+                  </b-link>
+                </span>
+              </b-form-checkbox>
+              <collapse-text-box bid="cond2-info">
+                  Information is missing
+              </collapse-text-box>
+              <div class="mt-5">
+                  <b-row align-h="between">
+                      <b-col md="4" lg="3" order-md="2">
                         <b-button
                           class="mb-3"
                           size="lg"
@@ -51,18 +54,18 @@
                           :disabled="selected.length != 2">
                             Agree
                         </b-button>
-                        </b-col>
-                        <b-col md="4" lg="3" order-md="1">
+                      </b-col>
+                      <b-col md="4" lg="3" order-md="1">
                         <b-button
                           class="hh-back-button"
                           size="lg" type="submit"
                           block variant="outline-secondary"
                           @click="getBackPage()">
                             Back
-                          </b-button>
-                        </b-col>
-                    </b-row>
-                </div>
+                        </b-button>
+                      </b-col>
+                  </b-row>
+              </div>
             </b-form>
         </template>
     </generic-page>
