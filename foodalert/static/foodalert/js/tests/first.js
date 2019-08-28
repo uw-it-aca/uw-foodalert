c32b141eb5e2ec2ad8d961a8546e03f826cff170
Sarthak Singh <ss269@uw.edu>
2019-08-28 14:37:04 -0700

    test with nightwatch and docker

---
+++ foodalert/static/foodalert/js/tests/first.js
@@ -0,0 +1,25 @@
+module.exports = {
+    'Host Welcome Page Test' : function (browser) {
+      browser.pause(5000)
+      browser
+        .url('http://food:8000/h/welcome')
+  
+      browser.waitForElementVisible('body')
+  
+      browser.getTitle(function(title) {
+        this.assert.equal(title, "Share your leftover event food - Hungry Husky");
+      });
+  
+      browser.click('button').pause(100)
+  
+      browser.getTitle(function(title) {
+        this.assert.equal(title, "Help us determine if your food is shareable");
+      });
+  
+      browser.back().pause(200).getTitle(function(title) {
+        this.assert.equal(title, "Share your leftover event food - Hungry Husky");
+      });
+  
+      browser.end();
+    }
+  };
