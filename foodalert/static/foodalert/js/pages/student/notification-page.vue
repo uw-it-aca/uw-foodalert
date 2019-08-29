ec65ccc301fd144517c8e453401db8f4124ac7f4
erinRochfort99 <erochfor@uw.edu>
2019-08-29 13:26:46 -0700

    uses email notif card

--- foodalert/static/foodalert/js/pages/student/notification-page.vue
+++ foodalert/static/foodalert/js/pages/student/notification-page.vue
@@ -36,7 +36,7 @@
           </b-container>
         </b-collapse>
       </div>
-      <notification-option accord_id="text" type="text"
+      <!--notification-option accord_id="text" type="text"
         label="Enter a new phone number"
         description="Carrier rates may apply" :subid="subid"
         :serverData="{ text: notif_info.sms_number,
@@ -53,6 +53,7 @@
           Please reply YES to finish signup. <br />
         </template>
       </notification-option>
+
       <notification-option accord_id="email" type="email"
         label="Enter an email" :subid="subid"
         :serverData="{ text: notif_info.email,
@@ -69,7 +70,42 @@
           </b-button>. <br />
           Check your spam folder if you don't receive our email. <br />
         </template>
-      </notification-option>
+      </notification-option -->
+
+
+      <h2>Practice new notif design</h2>
+
+      <new-notif accord_id="text" type="text"
+        label="Enter an phone number" :subid="subid"
+        :serverData="{ text: notif_info.sms_number,
+          verified: notif_info.number_verified }"
+        :requestUpdate="requestUpdate"
+        :resendVerif="()=>{return 1}">
+        <template #opt_heading>
+          Text
+        </template>
+        <template #unverifNotifText="{switchToUpdate}">
+          We sent a verification text to
+          <b-button variant="link" @click="switchToUpdate" class="px-0">
+            {{notif_info.sms_number}}
+          </b-button>. <br />
+          Check your spam folder if you don't receive our email. <br />
+        </template>
+      </new-notif>
+
+      <email-notif accord_id="email" type="email"
+        label="Enter an email" :subid="subid"
+        :requestUpdate="requestUpdate"
+        :email="netID+'@uw.edu'">
+        <template #opt_heading>
+          Email
+        </template>
+        <template #disclaimer>
+          <p>Only UW NetIDs are support at this time</p>
+        </template>
+      </email-notif>
+
+      
     </template>
   </generic-page>
 </template>
@@ -77,12 +113,17 @@
 <script type="text/javascript">
 import GenericPage from '../../components/generic-page.vue';
 import NotifOption from '../../components/notification-option.vue';
+import new_notif from '../../components/new_notif_option.vue';
+import email_notif from '../../components/email_notif_option.vue';
 const axios = require('axios');
+import Cookies from 'js-cookie';
 
 export default {
   components: {
     'generic-page': GenericPage,
     'notification-option': NotifOption,
+    'new-notif': new_notif,
+    'email-notif': email_notif,
   },
   props: {
     bid: String,
@@ -120,35 +161,22 @@ export default {
     },
     requestUpdate() {
       axios.get('/subscription/?netID=' + this.netID)
-          .then(this.getSubID)
-          .then((data) => {
-            if (this.subid) {
-              const url = '/subscription/' + this.subid + '/';
-              axios.get(url)
-                  .then((response) => {
-                    response.data = this.formatSms(response.data);
-                    this.notif_info = response.data;
-
-                    // Control the b-collapse
-                    if (this.notif_info.sms_number != '' ||
-                      this.notif_info.email != '') {
-                      this.collapse_notif = true;
-                      if (this.notif_info.email_verified ||
-                        this.notif_info.number_verified) {
-                        this.disableNotif = false;
-                      } else {
-                        this.disableNotif = true;
-                      }
-                    } else {
-                      this.collapse_notif = false;
-                    }
-                  })
-                  .catch((error) =>
-                    this.showErrorPage(error.response, 's-notifications'));
-            }
-          })
-          .catch((error) =>
-            this.showErrorPage(error.response, 's-notifications'));
+        .then(this.getSubID)
+        .then((data) => {
+          if (this.subid) {
+            const url = '/subscription/' + this.subid + '/';
+            axios.get(url)
+              .then((response) => {
+                response.data = this.formatSms(response.data);
+                this.notif_info = response.data;
+              })
+              .catch((error) =>
+                this.showErrorPage(error.response, 's-notifications'));
+          }
+        })
+        .catch((error) => {
+          this.showErrorPage(error.response, 's-notifications')
+        });
     },
   },
   beforeMount() {
