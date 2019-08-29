b6af4b79dd0d548437e344ec0b0a1cc07951cb0e
Sarthak Singh <ss269@uw.edu>
2019-08-28 17:14:12 -0700

    Changed assert methods and removed wait

--- foodalert/static/foodalert/js/tests/first.js
+++ foodalert/static/foodalert/js/tests/first.js
@@ -1,24 +1,23 @@
 module.exports = {
   'Host Welcome Page Test': function(browser) {
-    browser.pause(50000);
     browser
         .url('http://localhost:8000/h/welcome');
 
     browser.waitForElementVisible('body');
 
     browser.getTitle(function(title) {
-      this.assert.equal(title, 'Share your leftover event food - Hungry Husky');
-    }.bind(this));
+      browser.assert.equal(title, 'Share your leftover event food - Hungry Husky');
+    });
 
     browser.click('button').pause(100);
 
     browser.getTitle(function(title) {
-      this.assert.equal(title, 'Help us determine if your food is shareable');
-    }.bind(this));
+      browser.assert.equal(title, 'Help us determine if your food is shareable');
+    });
 
     browser.back().pause(200).getTitle(function(title) {
-      this.assert.equal(title, 'Share your leftover event food - Hungry Husky');
-    }.bind(this));
+      browser.assert.equal(title, 'Share your leftover event food - Hungry Husky');
+    });
 
     browser.end();
   },
