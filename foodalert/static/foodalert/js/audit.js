e4e6899413d6af0a0fb4edd2a0514b5569c7ba9b
erinRochfort99 <erochfor@uw.edu>
2019-09-11 16:13:05 -0700

    deleted reference to audit controller

--- foodalert/static/foodalert/js/audit.js
+++ foodalert/static/foodalert/js/audit.js
@@ -1,10 +1,10 @@
 import {activatePage} from './utils.js';
-import AuditController from './components/audit-controller.vue';
+import AuditTemplate from './components/audit-template.vue';
 
 window.vueData.routes[0].children = window.vueData.routes[0].children.concat([
-  {path: 'a/audit', component: AuditController},
+  {path: 'a/audit', component: AuditTemplate},
 ]);
 
 const rootPath = window.vueData.routes.find((obj) => obj.path === '/a/');
 
-activatePage(rootPath, 'audit', AuditController, 'Audit Page');
+activatePage(rootPath, 'audit', AuditTemplate, 'Audit Page');
