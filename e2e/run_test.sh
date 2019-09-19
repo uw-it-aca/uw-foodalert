ad13d4e4a45b784391053be5f72f332d40944208
Sarthak Singh <ss269@uw.edu>
2019-09-19 10:19:36 -0700

    Fixed perm issues

--- e2e/run_test.sh
+++ e2e/run_test.sh
@@ -2,8 +2,17 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker-compose -f $DIR/docker-compose.yml run taster
-docker-compose -f $DIR/docker-compose.yml down
+if [ -n "$1" ]; then
+    case "$1" in
+ 
+    -b) USERID=$(id -u) GROUPID=$(id -g) docker-compose -f $DIR/docker-compose.yml build $2 ;;
 
-sudo chown -R $USER $DIR
-sudo chgrp -R $(id -g) $DIR
+    *) echo "Option $1 not recognized" ;;
+
+    esac
+ 
+    shift
+fi
+
+USERID=$(id -u) GROUPID=$(id -g) docker-compose -f $DIR/docker-compose.yml run taster
+USERID=$(id -u) GROUPID=$(id -g) docker-compose -f $DIR/docker-compose.yml down
