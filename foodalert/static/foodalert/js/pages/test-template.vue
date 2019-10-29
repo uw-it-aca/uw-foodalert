3964c18fee0ec507f705d4d2e62506a7d5712557
erinRochfort99 <erochfor@uw.edu>
2019-10-29 16:43:03 -0700

    eslint fix

--- foodalert/static/foodalert/js/pages/test-template.vue
+++ foodalert/static/foodalert/js/pages/test-template.vue
@@ -1,23 +1,11 @@
 <template>
   <div>
-    <b-navbar class="mx-auto" style="height: 64px;">
-      <b-navbar-brand class="pl-2 pb-2">
-        <img :src="require('../../img/food-alert-logo.svg')"
-              alt="UW Food Alert Logo" height="22"
-              >
-      </b-navbar-brand>
-    </b-navbar>
-    <audit-template></audit-template>
   </div>
 </template>
 
 <script type="text/javascript">
-import AuditTemplate from '../components/audit-template.vue';
 
 export default {
-  components: {
-    'audit-template': AuditTemplate,
-  },
 
   data() {
     return {
