b347fe4691bb9b33e597e48db82337b5c0ec41fd
erinRochfort99 <erochfor@uw.edu>
2020-02-06 15:15:19 -0800

    deleted duplicate test

--- foodalert/test/test_twilio.py
+++ foodalert/test/test_twilio.py
@@ -175,7 +175,7 @@ class TwilioTest(TestCase):
     @override_settings(TWILIO_ACCOUNT_SID="test_sid")
     def test_reply_stop_and_start(self):
         """
-        Tests replies recivied form twilio
+        Tests STOP/START behavior
         """
         sub_user = create_user_from_data({
             "username": "test_sub",
@@ -203,6 +203,7 @@ class TwilioTest(TestCase):
 
             sub.refresh_from_db()
             self.assertFalse(sub.send_sms)
+            self.assertTrue(sub.twilio_stop)
 
             # test start
             response = client.post('/api/v1/sms/', data={
@@ -214,52 +215,7 @@ class TwilioTest(TestCase):
 
             sub.refresh_from_db()
             self.assertTrue(sub.send_sms)
-
-        sub.delete()
-        sub_user.delete()
-
-    @override_settings(TWILIO_ACCOUNT_SID="test_sid")
-    def test_reply_stop_and_start(self):
-        """
-        Tests replies recivied form twilio
-        """
-        sub_user = create_user_from_data({
-            "username": "test_sub",
-            "email": "test_sub@uw.edu",
-            "password": "test_password"
-        })
-        sub = Subscription.objects.create(
-            user=sub_user,
-            email=sub_user.email,
-            sms_number="+41524204242",
-            number_verified=True,
-            send_sms=True
-        )
-
-        with generate_twilio_request_validator_mock():
-            client = Client()
-
-            # test stop
-            response = client.post('/api/v1/sms/', data={
-                'AccountSid': 'test_sid',
-                'From': str(sub.sms_number),
-                'Body': 'Stop'
-            }, HTTP_X_TWILIO_SIGNATURE='unknown_value')
-            self.assertEqual(response.status_code, 200)
-
-            sub.refresh_from_db()
-            self.assertFalse(sub.send_sms)
-
-            # test start
-            response = client.post('/api/v1/sms/', data={
-                'AccountSid': 'test_sid',
-                'From': str(sub.sms_number),
-                'Body': 'Start'
-            }, HTTP_X_TWILIO_SIGNATURE='unknown_value')
-            self.assertEqual(response.status_code, 200)
-
-            sub.refresh_from_db()
-            self.assertTrue(sub.send_sms)
+            self.assertFalse(sub.twilio_stop)
 
         sub.delete()
         sub_user.delete()
