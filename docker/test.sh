276cfd03b000dee02eaa80ed2a48ec487d59e146
Jonathan Beaubien <beaubj@uw.edu>
2021-10-18 12:23:55 -0700

    Change 3

--- docker/test.sh
+++ docker/test.sh
@@ -12,7 +12,7 @@ trap catch ERR
 source bin/activate
 
 # install test tooling
-npm install eslint-plugin-vue -g
+npm install eslint-plugin-vue eslint-config-google eslint-plugin-vue-a11y eslint-config-stylelint -g
 
 function run_test {
     echo "##########################"
