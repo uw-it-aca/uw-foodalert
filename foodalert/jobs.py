ed8ec0e539dfdec448ab04b29d523377939f4354
Jonathan Beaubien <beaubj@uw.edu>
2022-02-15 12:03:14 -0800

    Fixed some pycodestyle problems2

--- foodalert/jobs.py
+++ foodalert/jobs.py
@@ -10,81 +10,81 @@ debug_mode = getattr(settings, 'DEBUG')
 
 logger = logging.getLogger(__name__)
 
+
 def queue_messages(job):
 
+    # Retreive users email/sms Subscription status
+    email_recipients = []
+    sms_recipients = []
+    for sub in Subscription.objects.all():
+        if sub.send_email:
+            email_recipients.append(sub.email)
+        if sub.send_sms:
+            sms_recipients.append(str(sub.sms_number))
 
-	# Retreive users email/sms Subscription status
-	email_recipients = []
-	sms_recipients = []
-	for sub in Subscription.objects.all():
-		if sub.send_email:
-			email_recipients.append(sub.email)
-		if sub.send_sms:
-			sms_recipients.append(str(sub.sms_number))
+    # Handle the formatting of messages based on
+    # if the message is for a Notifcation or Update
+    data = job.workspace['data']
+    email = {}
+    sms = ''
+    slug = ''
 
-	# Handle the formatting of messages based on
-	# if the message is for a Notifcation or Update
-	data = job.workspace['data']
-	email = {}
-	sms = ''
-	slug = ''
-	
-	if job.workspace['update']:
-		# Update
+    if job.workspace['update']:
+        # Update
 
-		# Email formatting
-		email['name'] = "Food Alert Update"
-		subj = "[UW Food Alert] Update: {}, {}"
-		email['subject'] = subj.format(job.workspace['event'],
-										job.workspace['location'])
-		email['body'] = ('Update: ' + data['text'] + '\n\n'
-						'Thanks,\n'
-						'UW Food Alert')
+        # Email formatting
+        email['name'] = "Food Alert Update"
+        subj = "[UW Food Alert] Update: {}, {}"
+        email['subject'] = subj.format(job.workspace['event'],
+                                        job.workspace['location'])
+        email['body'] = ('Update: ' + data['text'] + '\n\n'
+                        'Thanks,\n'
+                        'UW Food Alert')
 
-		# SMS formatting
-		sms = job.workspace['event'] + ' Update: ' + data['text']
+        # SMS formatting
+        sms = job.workspace['event'] + ' Update: ' + data['text']
 
-		# Time
-		slug = data['created_time']
-		
-	else:
-		# Notification
+        # Time
+        slug = data['created_time']
+        
+    else:
+        # Notification
 
-		# Email formatting
-		email['name'] = "Food Alert Notification"
-		subj = "[UW Food Alert] Food Available: {}, {}"
-		email['subject'] = subj.format(data['event'], data['location'])
-		data['time']['end'] = parser.parse(data['time']['end'])
-		email['body'] = Sender.format_message(data)
+        # Email formatting
+        email['name'] = "Food Alert Notification"
+        subj = "[UW Food Alert] Food Available: {}, {}"
+        email['subject'] = subj.format(data['event'], data['location'])
+        data['time']['end'] = parser.parse(data['time']['end'])
+        email['body'] = Sender.format_message(data)
 
-		# SMS formatting
-		sms = email['body'] # Text message is the same as the body of the email
+        # SMS formatting
+        sms = email['body'] # Text message is the same as the body of the email
 
-		# Time
-		slug = data['time']['created']
+        # Time
+        slug = data['time']['created']
 
-	for ch in [' ', ':', '+']:
-		slug = slug.replace(ch, '')
+    for ch in [' ', ':', '+']:
+        slug = slug.replace(ch, '')
 
-	# Queue up the actual sending of email/sms
-	Job.objects.create(name='send_email',
-					   workspace={'recipients': email_recipients,
-								  'email': email,
-								  'time': slug})
+    # Queue up the actual sending of email/sms
+    Job.objects.create(name='send_email',
+                        workspace={'recipients': email_recipients,
+                                    'email': email,
+                                    'time': slug})
 
-	Job.objects.create(name='send_sms',
-					   workspace={'recipients': sms_recipients,
-								  'sms': sms})
+    Job.objects.create(name='send_sms',
+                        workspace={'recipients': sms_recipients,
+                                    'sms': sms})
 
 def send_email(job):
-	Sender.send_email(job.workspace['email'],
-					  job.workspace['recipients'],
-					  job.workspace['time'])
+    Sender.send_email(job.workspace['email'],
+                        job.workspace['recipients'],
+                        job.workspace['time'])
 
 def send_sms(job):
-	if debug_mode:
-		logger.info('Not sending sms while in debug mode')
-		logger.info(job.workspace)
-	else:
-		Sender.send_twilio_sms(job.workspace['recipients'],
-							   job.workspace['sms'])
+    if debug_mode:
+        logger.info('Not sending sms while in debug mode')
+        logger.info(job.workspace)
+    else:
+        Sender.send_twilio_sms(job.workspace['recipients'],
+                                job.workspace['sms'])
