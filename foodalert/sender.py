739e63a43d5afb02d46984edd5bc447b564411ff
Craig M. Stimmel <cstimmel@uw.edu>
2022-02-22 11:22:30 -0800

    Remove copyright

--- foodalert/sender.py
+++ foodalert/sender.py
@@ -1,6 +1,3 @@
-# Copyright 2022 UW-IT, University of Washington
-# SPDX-License-Identifier: Apache-2.0
-
 # Copyright 2021 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 import json
