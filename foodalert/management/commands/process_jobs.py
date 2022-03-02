ddb60e026da7a03327bf2cca639280a0839b9809
Jonathan Beaubien <beaubj@uw.edu>
2022-03-01 16:40:56 -0800

    Added yml values for management_daemon and changed name

--- foodalert/management/commands/process_jobs.py
+++ foodalert/management/commands/process_jobs.py
@@ -1,7 +1,7 @@
 # Copyright 2022 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django_dbq.models import Job
 from django_dbq.management.commands.worker import process_job
 from django_dbq.management.commands.worker import DEFAULT_QUEUE_NAME
