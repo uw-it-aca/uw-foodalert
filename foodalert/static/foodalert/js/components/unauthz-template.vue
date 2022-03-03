36efc92e7acf4817bbd5d44e19ea7c407b000ca1
Jonathan Beaubien <beaubj@uw.edu>
2022-03-03 15:00:15 -0800

    ESlint warnings

--- foodalert/static/foodalert/js/components/unauthz-template.vue
+++ foodalert/static/foodalert/js/components/unauthz-template.vue
@@ -2,8 +2,9 @@
   <div id=root>
   <b-container class="px-4 mt-3">
       <p>
-          <strong>You are not authorized to view this page.</strong>
-          If you believe this is an error, please contact support at the bottom of this page.
+        <strong>You are not authorized to view this page.</strong>
+        If you believe this is an error, please contact support
+        at the bottom of this page.
       </p>
   </b-container>
       <footer id="relative-footer" class="text-center" role="contentinfo">
