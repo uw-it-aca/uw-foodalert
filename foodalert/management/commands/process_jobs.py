0fd562774eddbf87e51e4b73ec99a09a467bf5f8
Jim Laney <jlaney@uw.edu>
2023-06-06 08:31:15 -0700

    update actions, copyright

--- foodalert/management/commands/process_jobs.py
+++ foodalert/management/commands/process_jobs.py
@@ -1,6 +1,5 @@
-# Copyright 2022 UW-IT, University of Washington
+# Copyright 2023 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
-
 from django.core.management.base import BaseCommand
 from django_dbq.models import Job
 from django_dbq.management.commands.worker import process_job
--- foodalert/test/test_send_mail.py
+++ foodalert/test/test_send_mail.py
@@ -1,6 +1,5 @@
-# Copyright 2022 UW-IT, University of Washington
+# Copyright 2023 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
-
 from django.test import TestCase, Client
 from parameterized import parameterized, param
 from unittest.mock import patch
